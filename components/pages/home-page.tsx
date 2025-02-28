74c6871880e610daf997eae804dfdedda7cafa9b
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-02-28 23:20:28 +0000

    Build add new task form

--- components/pages/home-page.tsx
+++ components/pages/home-page.tsx
@@ -1,10 +1,61 @@
-import React from "react";
+"use client";
+
 import Header from "../organisms/header";
+import { Card, CardContent, CardHeader, CardTitle } from "../atoms/card";
+import { Button } from "../atoms/button";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "../atoms/form";
+import { Input } from "../atoms/input";
+
+const formSchema = z.object({
+  name: z.string().min(3),
+});
 
 export default function HomePage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <div>
+    <div className="flex flex-col gap-4 items-center">
       <Header />
+
+      <main className="w-full p-4">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Tasks</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="flex gap-2">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormControl>
+                        <Input placeholder="Add a new task..." autoComplete="off" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit">Add</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </main>
     </div>
   );
 }
