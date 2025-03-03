4bcb821764379324f519b3931d35ef750c2f95cf
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-03-03 23:44:19 +0000

    Rebuilt new task form component

--- components/organisms/new-task-form.tsx
+++ components/organisms/new-task-form.tsx
@@ -6,43 +6,56 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../atoms/form";
 import { Input } from "../atoms/input";
-import { CalendarIcon } from "lucide-react";
+import { CalendarIcon, Plus } from "lucide-react";
 import { Popover, PopoverContent, PopoverTrigger } from "../atoms/popover";
 import { Calendar } from "../atoms/calendar";
 import { Button } from "../atoms/button";
 
 import { toast } from "sonner";
 
-import axios from "axios";
-import { useRouter } from "next/navigation";
-
 import { format } from "date-fns";
-import { cn } from "@/lib/utils";
+
+import axios from "axios";
+
+import { Task } from "@prisma/client";
+import { Label } from "../atoms/label";
 
 const formSchema = z.object({
   name: z.string().min(3),
-  dueDate: z.date().min(new Date(new Date().setDate(new Date().getDate() - 1))),
+  dueDate: z
+    .union([
+      z.date(),
+      z.null(), // Allow null values
+    ])
+    .refine((date) => date === null || date >= new Date(new Date().setDate(new Date().getDate() - 1)), {
+      message: "Due date cannot be in the past",
+    }),
 });
+interface NewTaskFormProps {
+  tasks: Task[];
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+}
 
-export default function NewTaskForm() {
+export default function NewTaskForm({ tasks, setTasks }: NewTaskFormProps) {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      dueDate: new Date(),
+      dueDate: null,
     },
   });
 
-  const router = useRouter();
+  const selectedDate = form.watch("dueDate");
+  const isLoading = form.formState.isSubmitting;
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
-      const task = await axios.post("api/task/new", values);
+      const response = await axios.post("api/task/new", values);
+      const newTask = response.data;
 
+      setTasks([newTask, ...tasks]);
       toast("Task created.");
-
       form.reset();
-      router.refresh();
     } catch (error) {
       console.log(error);
     }
@@ -50,57 +63,60 @@ export default function NewTaskForm() {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem className="w-full">
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Add a new task..." autoComplete="off" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="dueDate"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel>Due date</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn("w-[240px] pl-3 text-left font-normal", !field.value && "text-muted-foreground")}
-                    >
-                      {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) => date < new Date(new Date().setDate(new Date().getDate() - 1))}
-                    initialFocus
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-2 mb-6">
+        <div className="flex gap-2">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Add a new task..."
+                    autoComplete="off"
+                    className="flex-1"
+                    disabled={isLoading}
+                    {...field}
                   />
-                </PopoverContent>
-              </Popover>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-        <Button type="submit" className="ml-auto">
-          Add
-        </Button>
+          <FormField
+            control={form.control}
+            name="dueDate"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button variant={"outline"} disabled={isLoading}>
+                        <CalendarIcon className="h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value ?? undefined}
+                      onSelect={field.onChange}
+                      disabled={(date) => date < new Date(new Date().setDate(new Date().getDate() - 1))}
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" size="icon" disabled={isLoading}>
+            <Plus className="h-4 w-4" />
+          </Button>
+        </div>
+
+        {selectedDate && <Label>Selected date: {format(selectedDate, "PPP")}</Label>}
       </form>
     </Form>
   );
