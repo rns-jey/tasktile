595c157007f7ce9b4fee61a0e6fb345a2f3de777
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-03-06 05:36:08 +0000

    Clean up code

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,3 @@
-import Link from "next/link";
-import { ArrowRight, CheckCircle, Diamond, Grid3X3, Layers, Sparkles } from "lucide-react";
-
-import { Button } from "@/components/atoms/button";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import HomePage from "@/components/pages/home-page";
 import LandingPage from "@/components/pages/landing-page";
