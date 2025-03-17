d28e9c1c9a4cd2980ecfc774dd465b5a17df546f
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-03-17 05:55:34 +0000

    Use parallel fetching to improve performance

--- components/organisms/task-section.tsx
+++ components/organisms/task-section.tsx
@@ -1,16 +1,24 @@
-import currentProfile from "@/lib/current-profile";
-import TaskList from "./task-list";
 import { db } from "@/lib/db";
+import TaskList from "./task-list";
+import currentProfile from "@/lib/current-profile";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function TaskSection() {
   const profile = await currentProfile();
 
-  if (!profile) return null;
+  if (!profile) return (await auth()).redirectToSignIn();
 
   const [tasks, categories] = await Promise.all([
-    db.task.findMany({ where: { userId: profile.id }, include: { category: true }, orderBy: [{ createdAt: "desc" }] }),
-    db.category.findMany({ where: { userId: profile.id } }),
+    db.task.findMany({
+      where: { userId: profile.id },
+      include: { category: true },
+      orderBy: [{ createdAt: "desc" }],
+    }),
+
+    db.category.findMany({
+      where: { userId: profile.id },
+    }),
   ]);
 
-  return <TaskList taskList={tasks} categoryList={categories} />;
+  return <TaskList tasks={tasks} categories={categories} />;
 }
