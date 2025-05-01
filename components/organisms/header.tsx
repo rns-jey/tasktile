67e6618acd415a20c4f3534bd864245f11717404
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-05-01 15:00:00 +0100

    Fix header alignment

--- components/organisms/header.tsx
+++ components/organisms/header.tsx
@@ -7,7 +7,7 @@ import { SignedOut } from "@clerk/nextjs";
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95">
-      <div className="container flex h-16 items-center justify-between space-x-4 px-4">
+      <div className="container mx-auto flex h-16 items-center justify-between space-x-4 px-4">
         <div className="flex gap-2 items-center text-xl font-bold">
           <Diamond className="h-6 w-6" />
           <span>TaskTile</span>
