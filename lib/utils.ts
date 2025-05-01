3c4dfe67a5debc3c9f6bc47b4c4a28eaec731213
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2025-05-01 17:13:30 +0100

    Build a generate month calendar util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { CalendarDay, RawContribution } from "@/types";
+import { clsx, type ClassValue } from "clsx";
+import { eachDayOfInterval, endOfMonth, endOfWeek, isSameDay, parseISO, startOfMonth, startOfWeek } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateMonthCalendar(contributions: RawContribution[], targetDate: Date = new Date()): CalendarDay[] {
+  const start = startOfWeek(startOfMonth(targetDate), { weekStartsOn: 0 });
+  const end = endOfWeek(endOfMonth(targetDate), { weekStartsOn: 0 });
+
+  const calendarDays = eachDayOfInterval({ start, end });
+
+  return calendarDays.map((day) => {
+    const match = contributions.find((item) => isSameDay(parseISO(item.date), day));
+
+    return {
+      date: day,
+      count: match ? match.count : 0,
+      isCurrentMonth: day.getMonth() === targetDate.getMonth(),
+    };
+  });
 }
