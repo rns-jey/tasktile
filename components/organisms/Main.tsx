f5e33b6b96e558fe0a95fd848dfa0aebaf8bdde3
Mikko Sanera <rns.jey@gmail.com>
2025-07-21 16:05:30 +0100

    Fix section heights

--- components/organisms/Main.tsx
+++ components/organisms/Main.tsx
@@ -10,7 +10,7 @@ export default function Main() {
   const pathname = usePathname();
 
   return (
-    <main className="flex w-full max-w-7xl flex-row gap-4 p-4">
+    <main className="flex w-full max-w-7xl flex-row items-start gap-4 p-4">
       <DashboardSection pathname={pathname} />
       {pathname === "/" || pathname === "/tasks" ? (
         <TaskSection />
