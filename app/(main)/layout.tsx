3186b18fdea3441f54641765fa8caed9ca273f26
Mikko Sanera <rns.jey@gmail.com>
2025-08-01 17:59:34 +0100

    Set app to dark mode

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactQueryProvider from "@/components/providers/react-query-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
-export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return <ReactQueryProvider>{children}</ReactQueryProvider>;
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <ReactQueryProvider>{children}</ReactQueryProvider>
+    </ThemeProvider>
+  );
 }
