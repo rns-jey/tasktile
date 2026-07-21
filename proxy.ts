9dee43fe73fdd354e91885ce005538441b9eb7d1
JM Arenas <rns.jey@gmail.com>
2026-07-21 17:58:30 +0100

    Fix protected routes

--- proxy.ts
+++ proxy.ts
@@ -20,12 +20,13 @@ import { auth } from "@/lib/auth/server";
 
 export default auth.middleware({
   // Redirects unauthenticated users to sign-in page
-  loginUrl: "/auth/sign-in",
+  loginUrl: "/sign-up",
 });
 
 export const config = {
   matcher: [
     // Protected routes requiring authentication
     // "/account/:path*",
+    "/",
   ],
 };
