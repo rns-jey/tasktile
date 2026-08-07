f6c0d25dbe59d26da23ab7a44679ca739976fbcd
JM Arenas <rns.jey@gmail.com>
2026-08-07 10:48:15 +0100

    Build a reusable sonner component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,15 @@
+import { Toaster } from "@/components/ui/Sonner";
 import React from "react";
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div className="h-full flex justify-center items-center">{children}</div>;
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <Toaster />
+      {children}
+    </div>
+  );
 }
